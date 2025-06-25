d1c66b674d07c1fd96de571e985445901c6d5cb2
gaddganepola <ganeypola@gmail.com>
2025-06-25 19:02:18 +0530

    need of interface example step 02 - to do coding developer needs a laptop

--- src/InterfaceNeedDemo.java
+++ src/InterfaceNeedDemo.java
@@ -1,12 +1,22 @@
+class Laptop {
+    public void coding(){
+        System.out.println("coding by using laptop");
+    }
+}
+
 class Developer {
-    public void devApp(){
-        System.out.println("dev app by coding");
+    public void devApp(Laptop lap){
+        lap.coding();
     }
 }
 
 public class InterfaceNeedDemo {
     public static void main(String[] args) {
+        //Before hire a developer need a laptop
+        Laptop lap = new Laptop();
+
+        //Hire developer
         Developer dev = new Developer();
-        dev.devApp();
+        dev.devApp(lap);
     }
 }
