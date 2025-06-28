55e4e15656f877a018cf5f9b3640328945bc2960
gaddganepola <ganeypola@gmail.com>
2025-06-28 09:36:28 +0530

    enum with if and switch

--- src/EnumDemo.java
+++ src/EnumDemo.java
@@ -9,5 +9,31 @@ public class EnumDemo {
         for (Status s : status) {
             System.out.println(s + " : " + s.ordinal());
         }
+
+        Status s = Status.Running;
+        if (s == Status.Running) {
+            System.out.println("Running");
+        } else if (s == Status.Failed) {
+            System.out.println("Failed");
+        } else if (s == Status.Pending) {
+            System.out.println("Pending");
+        } else {
+            System.out.println("Success");
+        }
+
+        switch (s) {
+            case Running:
+                System.out.println("Running");
+                break;
+            case Failed:
+                System.out.println("Failed");
+                break;
+            case Pending:
+                System.out.println("Pending");
+                break;
+            default:
+                System.out.println("Success");
+                break;
+        }
     }
 }
