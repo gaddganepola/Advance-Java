24e5a40837c55ed317f0e18d4dda43a146b6c754
gaddganepola <ganeypola@gmail.com>
2025-07-07 12:19:29 +0530

    get user input by buffered reader

--- src/UserInputsDemo.java
+++ src/UserInputsDemo.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class UserInputsDemo {
     public static void main(String[] args) {
@@ -8,9 +10,29 @@ public class UserInputsDemo {
         try {
             //Returns the ASCII value of a single character
             num = System.in.read();
+            // Clear leftover newline
+            System.in.read();
         } catch (IOException e) {
             System.out.println(e);
         }
-        System.out.println(num - 48);
+        System.out.println("The value from input stream: " + (num - 48));
+
+        //Use BufferedReader to get user inputs
+
+        System.out.println("Enter a number: ");
+
+        InputStreamReader in = new InputStreamReader(System.in);
+
+        BufferedReader br = new BufferedReader(in);
+
+        int i = 0;
+        try {
+            i = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("The value from BufferedReader: " + i);
+
     }
 }
