6f2547a51bbe46fe1ba57c31b1934437646db98b
gaddganepola <ganeypola@gmail.com>
2025-07-07 17:17:19 +0530

    optimize threads by using priority and sleep

--- src/ThreadsDemo.java
+++ src/ThreadsDemo.java
@@ -2,6 +2,12 @@ class C1 extends Thread {
     public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println("Hi");
+            //Optimize by using sleep
+            try {
+                Thread.sleep(11);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
@@ -9,6 +15,11 @@ class C2 extends Thread {
     public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
@@ -16,6 +27,14 @@ public class ThreadsDemo {
     public static void main(String[] args) {
         C1 c1 = new C1();
         C2 c2 = new C2();
+        //Get the thread priority
+        System.out.println(c1.getPriority());
+        System.out.println(c2.getPriority());
+        //Set the thread priority
+//        c1.setPriority(10);
+//        c2.setPriority(1);
+//        c2.setPriority(Thread.MAX_PRIORITY);
+
         c1.start();
         c2.start();
     }
