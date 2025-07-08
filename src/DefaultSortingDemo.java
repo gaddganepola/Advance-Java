49a30994e1e791dd1f92de974e896e500c302c9f
gaddganepola <ganeypola@gmail.com>
2025-07-08 11:01:06 +0530

    default sorting using collections class

---
+++ src/DefaultSortingDemo.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.sort;
+
+public class DefaultSortingDemo {
+    public static void main(String[] args) {
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(17);
+        nums.add(80);
+        nums.add(43);
+        nums.add(29);
+        nums.add(35);
+
+        System.out.println("Before sorting: ");
+        for (Integer num : nums) {
+            System.out.print(num + " ");
+        }
+        //Sort list using Collections class
+        Collections.sort(nums);
+
+        System.out.println("\nAfter sorting: ");
+        for (Integer num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
